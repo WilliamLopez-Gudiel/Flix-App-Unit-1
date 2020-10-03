8e79f5bacfcc65d4acea41607ba5aba887945690
casualwilly <32225842+casualwilly@users.noreply.github.com>
2020-10-03 02:43:18 -0400

    lab2 Added a poset synopsis and image grid for better User Experience, added trailer feature.

---
+++ Movies!/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Movies!
+//
+//  Created by William Gudiel on 9/30/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
